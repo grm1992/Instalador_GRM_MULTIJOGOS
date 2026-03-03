4c3b8d0a353b4319aa278ff1d8f344d1d71f5b90
guilhermeramosmartins-cloud <guilherme.ramos.martins@gmail.com>
2026-03-03 00:50:32 -0300

    Script completo com cores e validações

--- install-grm.sh
+++ install-grm.sh
@@ -5,7 +5,7 @@
 # By: (33) 991619949
 # ============================================
 
-# Cores
+# Cores - DEFINE ISSO AQUI PRIMEIRO CARALHO!
 VERM='\033[0;31m'
 VERD='\033[0;32m'
 AMAR='\033[1;33m'
@@ -13,9 +13,9 @@ AZUL='\033[0;34m'
 CIAN='\033[0;36m'
 RESET='\033[0m'
 
-# 👇 SEU USUÁRIO DO GITHUB
-GITHUB_USER="SEU_USUARIO"
-GITHUB_REPO="grm-multijogos"
+# Configuração do GitHub
+GITHUB_USER="grm1992"
+GITHUB_REPO="Instalador_GRM_MULTIJOGOS"
 
 clear
 echo -e "${CIAN}================================${RESET}"
@@ -48,20 +48,32 @@ if [ $? -eq 0 ]; then
     rm grm-commercial.tar.gz
     echo -e "${VERD}[OK] grm-commercial instalado!${RESET}"
 else
-    echo -e "${VERM}[ERRO] Falha no download${RESET}"
+    echo -e "${VERM}[ERRO] Falha no download do grm-commercial.tar.gz${RESET}"
     exit 1
 fi
 
 # Baixar batocera.conf
 echo -e "${AMAR}[3/5] Baixando batocera.conf...${RESET}"
 wget -q --show-progress "https://raw.githubusercontent.com/${GITHUB_USER}/${GITHUB_REPO}/main/batocera.conf" -O /userdata/system/batocera.conf
-echo -e "${VERD}[OK] batocera.conf instalado!${RESET}"
+
+if [ $? -eq 0 ]; then
+    echo -e "${VERD}[OK] batocera.conf instalado!${RESET}"
+else
+    echo -e "${VERM}[ERRO] Falha no download do batocera.conf${RESET}"
+    exit 1
+fi
 
 # Baixar emulationstation
 echo -e "${AMAR}[4/5] Baixando emulationstation...${RESET}"
 wget -q --show-progress "https://raw.githubusercontent.com/${GITHUB_USER}/${GITHUB_REPO}/main/emulationstation" -O /usr/bin/emulationstation
-chmod +x /usr/bin/emulationstation
-echo -e "${VERD}[OK] emulationstation instalado!${RESET}"
+
+if [ $? -eq 0 ]; then
+    chmod +x /usr/bin/emulationstation
+    echo -e "${VERD}[OK] emulationstation instalado!${RESET}"
+else
+    echo -e "${VERM}[ERRO] Falha no download do emulationstation${RESET}"
+    exit 1
+fi
 
 # Iniciar EmulationStation
 echo -e "${AMAR}[5/5] Iniciando EmulationStation...${RESET}"
@@ -69,7 +81,7 @@ echo -e "${AMAR}[5/5] Iniciando EmulationStation...${RESET}"
 
 echo ""
 echo -e "${VERD}================================${RESET}"
-echo -e "${VERd}  INSTALAÇÃO FINALIZADA PORRA!${RESET}"
+echo -e "${VERD}  INSTALAÇÃO FINALIZADA PORRA!${RESET}"
 echo -e "${VERD}================================${RESET}"
 echo -e "${CIAN}GRM MULTIJOGOS - (33) 991619949${RESET}"
 echo -e "${VERD}================================${RESET}"
